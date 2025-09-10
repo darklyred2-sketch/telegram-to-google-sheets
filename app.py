6d7fa3da0ab895c5aaaba431f5e86135a2564875
darklyred2-sketch <darklyred2@gmail.com>
2025-09-10 20:21:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,10 @@ def telegram_webhook():
                     "vacancy_Тестировщик": "https://docs.google.com/document/d/1WD-X-jStPjSgJvs9428u-eHU7QTr6Almc0IgFq3limM/edit?tab=t.0",
                     "vacancy_DEVOPS": "https://docs.google.com/document/d/1AbuouxaQLJsn9IpMMrbBuj0Dps6aUJD4gYm9pOduZA4/edit?tab=t.0",
                     "vacancy_FRONTEND": "https://docs.google.com/document/d/1nCk58gHYmR3475_n_IEN9HcnspWxxGbAnD55JxG6uSw/edit?tab=t.0"
+                    "vacancy_Архитектор": "https://docs.google.com/document/d/16qFb_OCqnEuy0xK0qpmMHZ-KKAbrhulKlFFxT7lSYiQ/edit?usp=sharing"
+                    "vacancy_PYPTON": "https://docs.google.com/document/d/1SK9gnwlB_znxQENpgO0FaPbeo9ryH1V5KaQktYN2jXc/edit?usp=sharing"
+                    "vacancy_DEVOPS GREENPLUM": "https://docs.google.com/document/d/1XC3no-zSn1yHJ5H5XKjSyXQ4qlhCYMJ2b2d_s16zwOk/edit?tab=t.0"
+                    "vacancy_DEV GREENPLUM": "https://docs.google.com/document/d/1v3MH3On2-nqsN2OHBeYyE1JEe4qcOK2CoFgL7c1gnGM/edit?tab=t.0"
                 }
                 url = vacancy_map.get(data, "Ссылка не найдена.")
                 send_telegram_message(chat_id, f"📄 Требования по вакансии: {url}")
@@ -84,6 +88,22 @@ def telegram_webhook():
                 template = (
                     "Позиция: SENIOR DEV PYTHON\n"
                     "Команда: DBAAS\n"
+                    "Соискатель: \n"
+                    "\n!Прикрепите резюме, напиши ФИО соискателя и поставь @ перед именем бота ниже!"
+                    "\nOutstaff_connect_bot"
+                )
+            elif data == "template_DEVOPS GREENPLUM":
+                template = (
+                    "Позиция: SENIOR DEVOPS GREENPLUM\n"
+                    "Команда: ARENADATADB\n"
+                    "Соискатель: \n"
+                    "\n!Прикрепите резюме, напиши ФИО соискателя и поставь @ перед именем бота ниже!"
+                    "\nOutstaff_connect_bot"
+                )
+            elif data == "template_DEV GREENPLUM":
+                template = (
+                    "Позиция: SENIOR DEV GREENPLUM\n"
+                    "Команда: ARENADATADB\n"
                     "Соискатель: \n"
                     "\n!Прикрепите резюме, напиши ФИО соискателя и поставь @ перед именем бота ниже!"
                     "\nOutstaff_connect_bot"
@@ -139,6 +159,8 @@ def telegram_webhook():
                 [{"text": "FRONTEND", "callback_data": "template_frontend"}],
                 [{"text": "Архитектор", "callback_data": "template_Архитектор"}],
                 [{"text": "PYTHON", "callback_data": "template_PYTHON"}]
+                [{"text": "DEVOPS GREENPLUM", "callback_data": "template_DEVOPS GREENPLUM"}]
+                [{"text": "DEV GREENPLUM", "callback_data": "template_Разработчик GREENPLUM"}]
             ]
             send_telegram_inline_keyboard(chat_id, "Выберите шаблон:", inline_keyboard)
             return jsonify({"status": "inline_template_sent"}), 200
@@ -149,6 +171,10 @@ def telegram_webhook():
                 [{"text": "Тестировщик", "callback_data": "vacancy_Тестировщик"}],
                 [{"text": "DEVOPS", "callback_data": "vacancy_DEVOPS"}],
                 [{"text": "FRONTEND", "callback_data": "vacancy_FRONTEND"}]
+                [{"text": "Архитектор", "callback_data": "vacancy_Архитектор"}]
+                [{"text": "PYTHON", "callback_data": "vacancy_PYTHON"}]
+                [{"text": "DEVOPS GREENPLUM", "callback_data": "vacancy_DEVOPS GREENPLUM"}]
+                [{"text": "DEV GREENPLUM", "callback_data": "vacancy_Разработчик GREENPLUM"}]
             ]
             send_telegram_inline_keyboard(chat_id, "выберете вакансию, по которой хотите посмотреть требования", inline_keyboard)
             return jsonify({"status": "inline_vacancy_sent"}), 200
