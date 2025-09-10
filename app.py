8750e484c9b930984b8b6e491ae5492321389b3d
darklyred2-sketch <darklyred2@gmail.com>
2025-09-10 17:03:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,8 @@ def telegram_webhook():
                 template = (
                     "Позиция: SENIOR SDET\n"
                     "Команда: DATAHUB\n"
-                    "Соискатель: \n"
+                    "Соискатель: \n\n"
+                    "\n[Описание вакансии](https://docs.google.com/document/d/1WD-X-jStPjSgJvs9428u-eHU7QTr6Almc0IgFq3limM/edit?tab=t.0)\n"
                     "\n!Прикрепите резюме, напиши ФИО соискателя и поставь @ перед именем бота ниже!"
                     "\nOutstaff_connect_bot"
                 )
